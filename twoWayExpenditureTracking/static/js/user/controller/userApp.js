7d81a1acd472b3cfa97d098e2a5cef427541d724
akshay <kinhikar.akshay32@gmail.com>
2016-01-12 11:44:52 +0530

    added url for acc detail

--- twoWayExpenditureTracking/static/js/user/controller/userApp.js
+++ twoWayExpenditureTracking/static/js/user/controller/userApp.js
@@ -3,13 +3,17 @@ var mudraApp = angular.module('mudraApp', ['ui.router']);
 mudraApp.config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/");
   //
   // Now set up the states
   $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: ""
+    .state('accList', {
+      url: "/acc_list",
+      templateUrl: "/static/html_templates/accountList.html"
+    })
+    .state('accDetail', {
+      url: "/accDetail",
+      templateUrl: "/static/html_templates/accDetail.html"
     });
 
  });
